48b77c4a13f252c3280ef5c6cd1b84f32d52f8fb
Roger  J Jones <rjones@mba1976.hbs.edu>
2020-08-05 11:55:38 -0700

    Add project files.

---
+++ Models/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoGalleryDBMaintenance.Models
+{
+    public class ApplicationDBContext
+    {
+    }
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext(DbContextOptions<BloggingContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Blog> Blogs { get; set; }
+    }
+}
